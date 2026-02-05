2eacb769a023387d4d96d22c61c47b6261f9bc1f
c0pper22 <kylesarsfield2222000@gmail.com>
2026-02-05 09:27:19 -0500

    version update

--- pmgen/updater/updater.py
+++ pmgen/updater/updater.py
@@ -13,7 +13,7 @@ from PyQt6.QtCore import QObject, pyqtSignal
 # --- CONFIGURATION ---
 GITHUB_REPO = "c0pper22/PmGen"
 ASSET_NAME = "PmGen.zip" 
-CURRENT_VERSION = "2.7.2"
+CURRENT_VERSION = "2.7.3"
 HEADERS = {'User-Agent': f"PmGen-Updater/{CURRENT_VERSION}"}
 
 class UpdateWorker(QObject):
