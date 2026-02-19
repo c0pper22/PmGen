526082cf6fa7e8cc59353da4fde0e572d56541d5
c0pper22 <kylesarsfield2222000@gmail.com>
2026-02-19 13:56:35 -0500

    version update

--- pmgen/updater/updater.py
+++ pmgen/updater/updater.py
@@ -14,7 +14,7 @@ from PyQt6.QtCore import QObject, pyqtSignal
 # --- CONFIGURATION ---
 GITHUB_REPO = "c0pper22/PmGen"
 ASSET_NAME = "PmGen.zip"
-CURRENT_VERSION = "2.8.1"
+CURRENT_VERSION = "2.8.2"
 USER_AGENT = f"PmGen-Updater/{CURRENT_VERSION}"
 UPDATER_EXE_NAME = "updater.exe"
 
